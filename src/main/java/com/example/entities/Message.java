62f3c1eacfbf737f8267f2358aa8e8c851adb15b
DESKTOP-OG94NNN\Admin <namitagothwal99@gmail.com>
2021-12-16 23:33:55 +0530

    added new property to Message entitiy

--- src/main/java/com/example/entities/Message.java
+++ src/main/java/com/example/entities/Message.java
@@ -17,9 +17,16 @@ public class Message {
     @GenericGenerator(name = "uuid",strategy = "uuid2")
     private String messageId;
     private String txtMessage;
+    private String review;
 
     public Message(String messageId, String txtMessage) {
         this.messageId = messageId;
         this.txtMessage = txtMessage;
     }
+    public Message(String messageId, String txtMessage,String review) {
+        this.messageId = messageId;
+        this.txtMessage = txtMessage;
+        this.review=review;
+    }
+
 }
